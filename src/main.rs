dc4330c31396b2d3b27655dbc792bb484bb286cc
SirEdvin <siredvin.dark@gmail.com>
2023-08-31 23:56:17 +0300

    Working on this

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-#[macro_use] extern crate rocket;
-use rocket::http::Status;
-use rocket::request::{Outcome, Request, FromRequest};
-use rocket::fs::TempFile;
-use rocket::data::{Limits, ToByteUnit};
+#[macro_use]
+extern crate rocket;
 use rocket::config::Config;
+use rocket::data::{Limits, ToByteUnit};
+use rocket::fs::NamedFile;
+use rocket::fs::TempFile;
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+use rocket::response::status::NotFound;
+use rocket::serde::{json::Json, Serialize};
+use rocket::tokio::io::AsyncReadExt;
 use rocket::Data;
-use rocket::serde::{Serialize, json::Json};
+use std::io::Error;
+use tokio::fs::File;
+use tokio::io::{AsyncWriteExt, BufWriter};
 
 mod storage;
 
@@ -37,36 +44,92 @@ impl<'r> FromRequest<'r> for ApiKey<'r> {
 
 #[derive(FromForm)]
 struct Upload<'f> {
-    file: TempFile<'f>
+    file: TempFile<'f>,
 }
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Files {
-  files: Vec<String>,
+    files: Vec<String>,
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Info {
+    latest: String,
+}
 
 #[post("/storage/<group>/<entity>", data = "<data>")]
-async fn post_file(group: &str, entity: &str, data: Data<'_>, _key: ApiKey<'_>) -> std::io::Result<()>{
-    data.open(1.mebibytes()).into_file(storage::generate_file_name(group, entity)).await?;
+async fn post_file(
+    group: &str,
+    entity: &str,
+    data: Data<'_>,
+    _key: ApiKey<'_>,
+) -> std::io::Result<()> {
+    let files = storage::collect_files(group, entity);
+    let newest_file = files.last();
+    let file_name = storage::generate_file_name(group, entity);
+    if newest_file.is_none() {
+        data.open(1.mebibytes()).into_file(file_name).await?;
+    } else {
+        let newest_file_name: String = files.last().unwrap().to_string();
+        let mut file_data = storage::extract_file(group, entity, newest_file_name).await?;
+        let mut old_data: Vec<u8> = Vec::new();
+        file_data.read_to_end(&mut old_data).await?;
+        let new_data = data.open(1.mebibytes()).into_bytes().await?;
+        if !new_data.is_complete() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Something is not quite right",
+            ));
+        }
+
+        if old_data == new_data.value {
+            return Ok(());
+        }
+        println!("Files are not equivalent (?), {}, {}", old_data.last().unwrap(), new_data.value.last().unwrap());
+        let mut new_file = File::create(file_name).await?;
+        let mut buffer = tokio::io::BufWriter::new(&mut new_file);
+        buffer.write(&new_data.value).await?;
+    }
     Ok(())
 }
 
 #[get("/storage/<group>/<entity>/versions")]
-async fn get_versions(group: &str, entity: &str, _key: ApiKey<'_>) -> Json<Files>{
-    return Json(Files{ files: storage::collect_files(group, entity) })
-} 
+async fn get_versions(group: &str, entity: &str) -> Json<Files> {
+    return Json(Files {
+        files: storage::collect_files(group, entity),
+    });
+}
 
-// #[get("/storage/<group>/<file>/info")]
-// fn index() -> &'static str {
-//     "Hello, world!"
-// }
+#[get("/storage/<group>/<entity>/info")]
+async fn get_info(group: &str, entity: &str) -> Json<Info> {
+    let files = storage::collect_files(group, entity);
+    let newest_file: String = files.last().unwrap_or(&String::from("1990")).to_string();
+    return Json(Info {
+        latest: newest_file.replace(".lua", ""),
+    });
+}
+
+#[get("/storage/<group>/<entity>/latest")]
+async fn get_latest(group: &str, entity: &str) -> Result<NamedFile, NotFound<String>> {
+    let files = storage::collect_files(group, entity);
+    let newest_file = files.last();
+    if newest_file.is_none() {
+        return Err(NotFound("there is no versions for this file".to_string()));
+    }
+    let newest_file_name: String = files.last().unwrap().to_string();
+    return storage::extract_file(group, entity, newest_file_name)
+        .await
+        .map_err(|e| NotFound(e.to_string()));
+}
 
 #[launch]
 fn rocket() -> _ {
     storage::ensure_dir();
     let mut config = Config::default();
     config.limits = Limits::default().limit("form", 1.mebibytes());
-    rocket::build().configure(config).mount("/", routes![post_file, get_versions])
+    rocket::build()
+        .configure(config)
+        .mount("/", routes![post_file, get_versions, get_info, get_latest])
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,53 +1,103 @@
 use chrono::{DateTime, Utc};
-use std::time::SystemTime;
+use rocket::fs::NamedFile;
 use std::fs;
-use std::path::Path;
+use std::io;
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
 
-static STORAGE_DIRECTORY: &str = "./data_storage";
+static STORAGE_DIRECTORY: &str = "data_storage";
 
 pub fn generate_file_name(group: &str, entity: &str) -> String {
-    let system_time =SystemTime::now();
+    let system_time = SystemTime::now();
     let now: DateTime<Utc> = system_time.into();
     ensure_group_and_entity_dir(group, entity);
-    return format!("./{}/{}/{}/{}.lua", STORAGE_DIRECTORY, group, entity, now.to_rfc3339())
+    let mut buf = PathBuf::new();
+    buf.push(".");
+    buf.push(STORAGE_DIRECTORY);
+    buf.push(group);
+    buf.push(entity);
+    buf.push(now.format("%Y-%m-%d-%H-%M-%S.lua").to_string());
+    return buf.to_string_lossy().to_string();
 }
 
 pub fn ensure_dir() {
-    let file_path = &format!("./{}", STORAGE_DIRECTORY);
-    let dir_path = Path::new(file_path);
-    if !dir_path.exists() {
-        fs::create_dir(dir_path).unwrap()
+    let mut buf = PathBuf::new();
+    buf.push(".");
+    buf.push(STORAGE_DIRECTORY);
+    if !buf.exists() {
+        fs::create_dir(buf).unwrap()
     }
 }
 
 fn ensure_group_dir(group: &str) {
-    let file_path = &format!("./{}/{}", STORAGE_DIRECTORY, group);
-    let dir_path = Path::new(file_path);
-    if !dir_path.exists() {
-        fs::create_dir(dir_path).unwrap()
+    let mut buf = PathBuf::new();
+    buf.push(".");
+    buf.push(STORAGE_DIRECTORY);
+    buf.push(group);
+    if !buf.exists() {
+        fs::create_dir(buf).unwrap()
     }
 }
 
 fn ensure_group_and_entity_dir(group: &str, entity: &str) {
     ensure_group_dir(group);
-    let file_path = &format!("./{}/{}/{}", STORAGE_DIRECTORY, group, entity);
-    let dir_path = Path::new(file_path);
-    if !dir_path.exists() {
-        fs::create_dir(dir_path).unwrap()
+    let mut buf = PathBuf::new();
+    buf.push(".");
+    buf.push(STORAGE_DIRECTORY);
+    buf.push(group);
+    buf.push(entity);
+    if !buf.exists() {
+        fs::create_dir(buf).unwrap()
     }
 }
 
 pub fn collect_files(group: &str, entity: &str) -> Vec<String> {
     ensure_group_and_entity_dir(group, entity);
-    let file_path = &format!("./{}/{}/{}", STORAGE_DIRECTORY, group, entity);
-    let dir_path = Path::new(file_path);
-    let read_dir = std::fs::read_dir(dir_path).unwrap();
-    read_dir.filter_map(|x| -> Option<String> {
-        if x.is_err() { return None }
-        let file_name = x.unwrap().file_name().into_string();
-        if file_name.is_err() { return None }
-        let unwrapped_value = file_name.unwrap();
-        if unwrapped_value.ends_with(".lua") {return Some(unwrapped_value)}
-        return None
-    }).collect()
+    let mut buf = PathBuf::new();
+    buf.push(".");
+    buf.push(STORAGE_DIRECTORY);
+    buf.push(group);
+    buf.push(entity);
+    let read_dir = std::fs::read_dir(buf).unwrap();
+    let mut vec = read_dir
+        .filter_map(|x| -> Option<String> {
+            if x.is_err() {
+                return None;
+            }
+            let file_name = x.unwrap().file_name().into_string();
+            if file_name.is_err() {
+                return None;
+            }
+            let unwrapped_value = file_name.unwrap();
+            if unwrapped_value.ends_with(".lua") {
+                return Some(unwrapped_value);
+            }
+            return None;
+        })
+        .collect::<Vec<String>>();
+    vec.sort();
+    return vec;
+}
+
+fn build_file_name(group: &str, entity: &str, file_name: String) -> PathBuf {
+    let mut buf = PathBuf::new();
+    buf.push(".");
+    buf.push(STORAGE_DIRECTORY);
+    buf.push(group);
+    buf.push(entity);
+    buf.push(file_name);
+    buf
+}
+
+pub async fn extract_file(
+    group: &str,
+    entity: &str,
+    file_name: String,
+) -> std::io::Result<NamedFile> {
+    NamedFile::open(build_file_name(group, entity, file_name)).await
+}
+
+pub async fn cleanup(group: &str, entity: &str) {
+    let files = collect_files(group, entity);
+    files[..3]
 }
